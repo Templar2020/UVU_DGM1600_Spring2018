74e8fab7cb8d77fe56f02035d27341bad23290bb
Unknown <preston.powell@uvu.edu>
2018-01-25 19:25:50 -0700

    Added Player Controller

--- CallofChicken/Assets/Scripts/CubeMove.cs
+++ CallofChicken/Assets/Scripts/CubeMove.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
 	
-	public float transX;
-	public float transY;
-	public float transZ;
+	// public float transX;
+	// public float transY;
+	// public float transZ;
 
-	public float rotX;
-	public float rotY;
-	public float rotZ;
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
 
+		// public Vector3 move;
+		// public Vector3 rotate;
+
+		public float moveSpeed; 
+
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -19,7 +25,28 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(transX,transY,transZ);
-		transform.Rotate(rotX,rotY,rotZ);	
+		// transform.Translate(transX,transY,transZ);
+		// transform.Rotate(rotX,rotY,rotZ);
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.Q)){
+			transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.E)){
+			transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+		}
+
+		// transform.Translate(move * Time.deltaTime);
+		// transform.Rotate(rotate * Time.deltaTime);	
+
+		
+
 	}
 }
